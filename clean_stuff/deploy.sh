d05cb7b03eb765a8824deeb2e04dbf4f6b732e0e
Ajzner, Yeshayahu Dov <shaya@blazemeter.com>
2018-11-12 08:24:11 +0200

    g

--- clean_stuff/deploy.sh
+++ clean_stuff/deploy.sh
@@ -1,4 +1,4 @@
 cd ~/Desktop/repos/bzdev && source bzdev-venv/bin/activate && python bzdev.py env deploy --id  126
 kubectl scale deploy dpl-backend-worker-scheduler-126-ajzye01-shayablaze --replicas=5
-kubectl scale deploy dpl-backend-worker-126-ajzye01-shayablaze --replicas=100
+kubectl scale deploy dpl-backend-worker-126-ajzye01-shayablaze --replicas=200
 kubectl get deploy
