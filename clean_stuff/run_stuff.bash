221f51228b019d1e114e5d401fd68670fa2c6445
Ajzner, Yeshayahu Dov <shaya@blazemeter.com>
2018-11-18 14:46:03 +0200

    don't need this, have new route

--- clean_stuff/run_stuff.bash
+++ clean_stuff/run_stuff.bash
@@ -1,4 +1,4 @@
-python destroy_masters.py
+#python destroy_masters.py
 mongo --port 27019 < mongo_clean.sh
 redis-cli -p 6378 < redis_clean.sh
 cd /Users/ajzye01/Desktop/repos/productivity_scripts/clean_stuff
