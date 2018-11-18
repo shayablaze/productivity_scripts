034b412ddcd3ad644c50cf5d796a656ebce474b0
Ajzner, Yeshayahu Dov <shaya@blazemeter.com>
2018-11-18 15:16:35 +0200

    gwr

--- clean_stuff/terminate_by_env.sh
+++ clean_stuff/terminate_by_env.sh
@@ -1,4 +1,3 @@
 current_dir=$(dirname $0)
 . ${current_dir}/creds
-echo $user_info
 curl 'https://bza-126-ajzye01-shayablaze.env.blazemeter.net/api/v4/admin/tag-terminate?env=bza-126-ajzye01-shayablaze.env.blazemeter.net&locationId=ap-northeast-2' -X POST 'Content-Type: text/plain;charset=UTF-8' -H 'Accept: application/json' --user $user_info
