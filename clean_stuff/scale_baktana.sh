1cc624493d460414fde229932a79dfaba82a0863
Ajzner, Yeshayahu Dov <shaya@blazemeter.com>
2019-02-11 18:09:11 +0200

    athg

--- clean_stuff/scale_baktana.sh
+++ clean_stuff/scale_baktana.sh
@@ -1,3 +1,3 @@
-kubectl scale deploy dpl-backend-worker-scheduler-126-ajzye01-shayablaze --replicas=2
-kubectl scale deploy dpl-bza-126-ajzye01-shayablaze --replicas=2
-kubectl scale deploy dpl-backend-worker-126-ajzye01-shayablaze --replicas=20
+kubectl scale deploy dpl-backend-worker-scheduler-34-shayaa-gcp --replicas=2
+kubectl scale deploy dpl-bza-34-shayaa-gcp --replicas=2
+kubectl scale deploy dpl-backend-worker-34-shayaa-gcp --replicas=20
