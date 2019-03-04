4770652ae960b4ac6c310b961a81cb56abe9be39
Ajzner, Yeshayahu Dov <shaya@blazemeter.com>
2019-03-04 10:01:49 +0200

    change to env id 53

--- clean_stuff/scale_baktana.sh
+++ clean_stuff/scale_baktana.sh
@@ -1,3 +1,4 @@
-kubectl scale deploy dpl-backend-worker-scheduler-34-shayaa-gcp --replicas=2
-kubectl scale deploy dpl-bza-34-shayaa-gcp --replicas=2
-kubectl scale deploy dpl-backend-worker-34-shayaa-gcp --replicas=20
+kubectl scale deploy dpl-backend-worker-scheduler-53-shayaa-gcp --replicas=2
+kubectl scale deploy dpl-bza-53-shayaa-gcp --replicas=2
+kubectl scale deploy dpl-data-53-shayaa-gcp --replicas=2
+kubectl scale deploy dpl-backend-worker-53-shayaa-gcp --replicas=200
