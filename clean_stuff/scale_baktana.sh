b5fc490f953cb0f7449043a6c3b8c0f6d9dd1d8c
Ajzner, Yeshayahu Dov <shaya@blazemeter.com>
2019-03-28 15:37:51 +0200

    rrr

--- clean_stuff/scale_baktana.sh
+++ clean_stuff/scale_baktana.sh
@@ -1,4 +1,4 @@
-kubectl scale deploy dpl-backend-worker-scheduler-392-shayaa-gcp --replicas=2
-kubectl scale deploy dpl-bza-392-shayaa-gcp --replicas=2
-kubectl scale deploy dpl-data-392-shayaa-gcp --replicas=2
-kubectl scale deploy dpl-backend-worker-392-shayaa-gcp --replicas=200
+kubectl scale deploy dpl-backend-worker-scheduler-394-shayaa-gcp --replicas=2
+kubectl scale deploy dpl-bza-394-shayaa-gcp --replicas=2
+kubectl scale deploy dpl-data-394-shayaa-gcp --replicas=2
+kubectl scale deploy dpl-backend-worker-394-shayaa-gcp --replicas=200
