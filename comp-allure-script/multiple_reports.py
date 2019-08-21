bc652539f2ef249e54892211d6087bbc89e85284
shaya2468 <shayasms128@gmail.com>
2019-08-21 20:16:55 +0300

    trhethetrtur

--- comp-allure-script/multiple_reports.py
+++ comp-allure-script/multiple_reports.py
@@ -3,13 +3,13 @@ import json
 from utils import objectify_csv
 import os
 # os.remove('config/.DS_Store')
-all_files = os.listdir('config')
-
-# print(all_files)
+the_files = os.listdir('config')
+number_of_runs = len(the_files)
+print(number_of_runs)
 
 result = {}
 
-for file_name in all_files:
+for file_name in the_files:
     print(file_name)
     current_object = objectify_csv('config/' + file_name)
     for key,value in current_object.items():
@@ -22,8 +22,23 @@ for file_name in all_files:
 result_nice = []
 
 for key,value in result.items():
-    result_nice.append({ 'number_of_failures': value, 'test name': key,})
+    result_nice.append({ 'number_of_failures': value, 'test name': key})
 
 pd.read_json(json.dumps(result_nice)).to_csv('results/report_mulitple.csv')
+
+
+consistent = []
+flaky = []
+
+for elem in result_nice:
+    if elem.get('number_of_failures') == number_of_runs:
+        consistent.append(elem.get('test name'))
+    else:
+        flaky.append({'test name': elem.get('test name'), 'ratio':'{0}/{1}'.format(elem.get('number_of_failures'), number_of_runs) })
+pd.read_json(json.dumps(flaky)).to_csv('results/report_flaky.csv')
+pd.read_json(json.dumps(consistent)).to_csv('results/report_consistent.csv')
+
 print('finished')
+
+
 # print(result)
