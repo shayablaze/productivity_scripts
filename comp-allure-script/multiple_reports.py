a50886ffbea22d9df00a0df635bf065a4029290c
shaya2468 <shayasms128@gmail.com>
2019-08-22 14:04:40 +0300

    thryjtherhethetjryktuywr serga rg

--- comp-allure-script/multiple_reports.py
+++ comp-allure-script/multiple_reports.py
@@ -8,6 +8,22 @@ the_files = os.listdir('config')
 number_of_runs = len(the_files)
 print(number_of_runs)
 
+
+def find_flakiness(tests_list, type):
+    nicely_formatted_arr = []
+    consistent = []
+    flaky = []
+    for key,value in tests_list.items():
+        nicely_formatted_arr.append({'number_of_appearances': value, 'test name': key})
+    for elem in nicely_formatted_arr:
+        if elem.get('number_of_appearances') == number_of_runs:
+            consistent.append(elem.get('test name'))
+        else:
+            flaky.append({'test name': elem.get('test name'), 'ratio':'{0}/{1}'.format(elem.get('number_of_appearances'), number_of_runs) })
+    pd.read_json(json.dumps(flaky)).to_csv('results/report_flaky_{}.csv'.format(type))
+    pd.read_json(json.dumps(consistent)).to_csv('results/report_consistent_{}.csv'.format(type))
+
+
 tests_that_failed = {}
 tests_that_ran = {}
 tests_that_skipped = {}
@@ -23,46 +39,8 @@ for file_name in the_files:
         if value == 'skipped':
             initialize_arr_for_counting(tests_that_skipped, key)
 
-
-tests_that_failed_nicely_formatted = []
-
-for key,value in tests_that_failed.items():
-    tests_that_failed_nicely_formatted.append({'number_of_failures': value, 'test name': key})
-
-# pd.read_json(json.dumps(tests_that_failed_nicely_formatted)).to_csv('results/report_mulitple.csv')
-
-
-consistent_failures = []
-flaky = []
-
-for elem in tests_that_failed_nicely_formatted:
-    if elem.get('number_of_failures') == number_of_runs:
-        consistent_failures.append(elem.get('test name'))
-    else:
-        flaky.append({'test name': elem.get('test name'), 'ratio':'{0}/{1}'.format(elem.get('number_of_failures'), number_of_runs) })
-pd.read_json(json.dumps(flaky)).to_csv('results/report_flaky_failures.csv')
-pd.read_json(json.dumps(consistent_failures)).to_csv('results/report_consistent_failures.csv')
-
-
-
-########METAL RULES########
-tests_that_ran_nicely_formatted = []
-
-for key,value in tests_that_ran.items():
-    tests_that_ran_nicely_formatted.append({'number_of_runs': value, 'test name': key})
-
-consistent_runs = []
-flaky_runs = []
-
-for elem in tests_that_ran_nicely_formatted:
-    if elem.get('number_of_runs') == number_of_runs:
-        consistent_runs.append(elem.get('test name'))
-    else:
-        flaky_runs.append({'test name': elem.get('test name'), 'ratio':'{0}/{1}'.format(elem.get('number_of_runs'), number_of_runs) })
-pd.read_json(json.dumps(flaky_runs)).to_csv('results/report_flaky_runs.csv')
-pd.read_json(json.dumps(consistent_runs)).to_csv('results/report_consistent_runs.csv')
+find_flakiness(tests_that_failed, 'failures')
+find_flakiness(tests_that_ran, 'runs')
 
 print('finished')
 
-
-# print(result)
