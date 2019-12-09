ec8bda2293f2d07e483a1f9717032db9a8516495
shayablaze <YeshayahuDov.Ajzner@ca.com>
2019-12-09 09:28:20 +0200

    sdgawrgwrg

--- c_proj/dynamic_linking/dns_override/taurus-cloud-mine/do_stuff.sh
+++ c_proj/dynamic_linking/dns_override/taurus-cloud-mine/do_stuff.sh
@@ -1,8 +1,8 @@
- make clean
+# make clean
  make
  cp libhostspriv.so /usr/lib/
- export LD_PRELOAD=/usr/lib/libhostspriv.so
-# echo "127.0.0.1 shaya_1" >> ~/aaa
- echo "127.0.0.1 shaya_1" >> aaa
- curl shaya_1
- curl shaya_2
+# export LD_PRELOAD=/usr/lib/libhostspriv.so
+# echo "127.0.0.1 shaya_1" >> ~/aaa // don't need anymore
+# echo "127.0.0.1 shaya_1" >> aaa
+# curl shaya_1
+# curl shaya_2
