1590270022c209f9f95276c7881825c65c160691
shayablaze <YeshayahuDov.Ajzner@ca.com>
2022-11-02 12:11:34 +0200

    stufff

---
+++ torero-mig/migrate.py
@@ -0,0 +1,30 @@
+from jproperties import Properties
+
+from datetime import datetime
+
+from pymongo import MongoClient
+
+p = Properties()
+with open("props", "rb") as f:
+    p.load(f, "utf-8")
+user = p.properties['user']
+password = p.properties['password']
+database_name =  p.properties['database_name']
+mongo_string = f'mongodb+srv://{user}:{password}@{database_name}'
+
+cluster = MongoClient(mongo_string)
+
+db = cluster['blazemeter']
+collection = db["masterSessions"]
+
+start = datetime(2022, 10, 25, 7, 51, 4)
+
+temp = collection.find( {"$and":[ {"_id": 65008935.0}, {"created": {"$gte":start} }]}  )
+for doc in temp:
+    print(doc)
+print('done')
+
+
+
+
+
