a61a2fa135ecacb2ef61a1f9f4c17f476ea31db0
shayablaze <YeshayahuDov.Ajzner@ca.com>
2022-11-02 13:10:39 +0200

    stufff

--- torero-mig/migrate.py
+++ torero-mig/migrate.py
@@ -11,7 +11,7 @@ with open("props", "rb") as f:
     p.load(f, "utf-8")
 user = p.properties['user']
 password = p.properties['password']
-database_name =  p.properties['database_name']
+database_name = p.properties['database_name']
 mongo_string = f'mongodb+srv://{user}:{password}@{database_name}'
 
 cluster = MongoClient(mongo_string)
@@ -21,13 +21,8 @@ collection = db["masterSessions"]
 
 start = datetime(2022, 10, 25, 7, 51, 4)
 
-temp = collection.find( {"$and":[  {"created": {"$gte":start} }]}  )
+temp = collection.find({"$and": [{"created": {"$gte": start}}]})
 for doc in temp:
     print(doc)
     time.sleep(1)
 print('done')
-
-
-
-
-
