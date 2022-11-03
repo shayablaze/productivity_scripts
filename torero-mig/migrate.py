a3d3d4fc894a7296c877a51dbd8c91d1ef3f3c6d
shayablaze <YeshayahuDov.Ajzner@ca.com>
2022-11-03 11:26:50 +0200

    stufff

--- torero-mig/migrate.py
+++ torero-mig/migrate.py
@@ -32,7 +32,9 @@ collection = db["masterSessions"]
 
 start = datetime(2022, 11, 3, 5, 51, 4)
 
-temp = collection.find( {"$and":[{"created": {"$gte":start} }]}  )
+query = {"$and":[{"_id": {"$in":[65116796, 65116801, 65116804, 65117337, 65118005, 65118135]} }]}
+
+temp = collection.find( query )
 
 tests = []
 multi_tests = []
@@ -48,13 +50,16 @@ print('before iterating results')
 i = 1
 
 print('calculating count for test')
-number_of_documents = collection.count_documents({"$and":[{"created": {"$gte":start} }]})
+number_of_documents = collection.count_documents(query)
 print(f'we got {number_of_documents} masters')
 
 if number_of_documents == 0:
     print('good bye')
     exit()
 
+test_info = {
+
+}
 for doc in temp:
 
     now = datetime.now()
@@ -65,89 +70,151 @@ for doc in temp:
     i+=1
     if 'test' in doc:
         # print(doc['test'])
-        if doc['test'] not in tests:
-            count_tests+=1
-        tests = list(set(tests) | set([doc['test']]))
-    elif 'testCollection' in doc:
-        # print(doc['test'])
 
-        if doc['testCollection'] not in multi_tests:
-            count_tests+=1
-        multi_tests = list(set(multi_tests) | set([doc['testCollection']]))
-        # print(tests)
+        project = doc['project']
+        if project not in test_info:
+            test_info[project] = { 'tests' : [], 'testCollection':[]  }
+        temp_tests = test_info[project]['tests']
+        temp_tests = list(set(temp_tests) | set([doc['test']]))
+        test_info[project]['tests'] = temp_tests
+
+    elif 'testCollection' in doc:
+
+        project = doc['project']
+        if project not in test_info:
+            test_info[project] = { 'tests' : [], 'testCollection':[]  }
+
+        temp_tests = test_info[project]['testCollection']
+        temp_tests = list(set(temp_tests) | set([doc['testCollection']]))
+        test_info[project]['testCollection'] = temp_tests
+
     else:
         master_id = doc['_id']
         # print(f'no test wtf with master: {master_id}')
         masters_with_no_test = list(set(masters_with_no_test) | set([doc['_id']]))
         count_master_no_test +=1
-print('after iterating results')
 
-print('here are multi tests')
-print(multi_tests)
+projects = list(test_info.keys())
 
-collection = db["testCollections"]
+collection = db["projects"]
 
 
-temp = collection.find( {"$and":[{"_id": {"$in":multi_tests} }]}  )
-print('here are the test collections from db')
+query = {"$and":[{"_id": {"$in":projects} }]}
 
-print('tests before')
-print(len(tests))
-print('calculating count for multi test')
-number_of_documents = collection.count_documents({"$and":[{"_id": {"$in":multi_tests} }]})
-print(f'we got {number_of_documents} multi tests')
+temp = collection.find( query )
 
+workspaces = []
+test_info_by_workspace = {}
 for doc in temp:
-    single_tests_from_multi_test = list(map(lambda x: x['testId'], doc['testsForExecutions']))
-    tests = list(set(tests) | set(single_tests_from_multi_test))
-print('tests after')
-print(len(tests))
-    # print(doc)
-i = 1
+    workspace_id = doc['workspace']
+    workspaces = list(set(workspaces) | set([workspace_id]))
+    project_id = doc['_id']
+    if workspace_id not in test_info_by_workspace:
+        test_info_by_workspace [workspace_id] = { 'tests' : [], 'testCollection':[]  }
+
+    test_info_by_workspace [workspace_id]['tests']+= test_info[project_id]['tests']
+    test_info_by_workspace [workspace_id]['testCollection'] += test_info[project_id]['testCollection']
+
+print(test_info)
+print(len(list(test_info.keys())))
+print(test_info_by_workspace)
+print(len(list(test_info_by_workspace.keys())))
+print(workspaces)
 
 
+collection = db["accounts"]
 
 
+query = {"$and":[{"workspaces": {"$in":workspaces} }]}
 
-collection = db["tests"]
-temp = collection.find( {"$and":[{"_id": {"$in":tests} } ,   {"configuration.scriptType" : { "$in": ["jmeter", "taurus"]  }}     ]}  )
+temp = collection.find( query )
 
-print('calculating count for test AFTER excluding non jmeter')
-number_of_documents = collection.count_documents({"$and":[{"_id": {"$in":tests} } ,   {"configuration.scriptType" : { "$in": ["jmeter", "taurus"]  }}     ]})
-
-final_test_ids = []
+accounts = []
+test_info_by_account = {}
 for doc in temp:
-    final_test_ids.append(doc['_id'])
+    account_id = doc['_id']
+    workspaces_from_account = doc['workspaces']
+    for workspace_id in workspaces_from_account:
+        if workspace_id in test_info_by_workspace:
+            if account_id not in test_info_by_account:
+                test_info_by_account [account_id] = { 'tests' : [], 'testCollection':[]  }
+            test_info_by_account [account_id]['tests']+= test_info_by_workspace[workspace_id]['tests']
+            test_info_by_account [account_id]['testCollection'] += test_info_by_workspace[workspace_id]['testCollection']
 
 
+print(len(list(test_info_by_account.keys())))
+print(test_info_by_account)
+print('done rose')
 
 
-
-
-for test in tests:
-    now = datetime.now()
-
-    current_time = now.strftime("%H:%M:%S")
-    # print(f'in MULTITEST iteration {i} out of {number_of_documents} with time {current_time}')
-    i+=1
-    f.write(f'{test}\n')
-# print('no tests in these masters')
-# print(masters_with_no_test)
+# print('after iterating results')
 #
-# print('count of tests')
-# print(count_tests)
+# print('here are multi tests')
+# print(multi_tests)
 #
-# print('count of no tests')
-# print(count_master_no_test)
-f.close()
-
-now = datetime.now()
-
-current_time = now.strftime("%H:%M:%S")
-print("after time =", current_time)
-print('done')
-
-
-
-
-
+# collection = db["testCollections"]
+#
+#
+# temp = collection.find( {"$and":[{"_id": {"$in":multi_tests} }]}  )
+# print('here are the test collections from db')
+#
+# print('tests before')
+# print(len(tests))
+# print('calculating count for multi test')
+# number_of_documents = collection.count_documents({"$and":[{"_id": {"$in":multi_tests} }]})
+# print(f'we got {number_of_documents} multi tests')
+#
+# for doc in temp:
+#     single_tests_from_multi_test = list(map(lambda x: x['testId'], doc['testsForExecutions']))
+#     tests = list(set(tests) | set(single_tests_from_multi_test))
+# print('tests after')
+# print(len(tests))
+#     # print(doc)
+# i = 1
+#
+#
+#
+#
+#
+# collection = db["tests"]
+# temp = collection.find( {"$and":[{"_id": {"$in":tests} } ,   {"configuration.scriptType" : { "$in": ["jmeter", "taurus"]  }}     ]}  )
+#
+# print('calculating count for test AFTER excluding non jmeter')
+# number_of_documents = collection.count_documents({"$and":[{"_id": {"$in":tests} } ,   {"configuration.scriptType" : { "$in": ["jmeter", "taurus"]  }}     ]})
+#
+# final_test_ids = []
+# for doc in temp:
+#     final_test_ids.append(doc['_id'])
+#
+#
+#
+#
+#
+#
+# for test in tests:
+#     now = datetime.now()
+#
+#     current_time = now.strftime("%H:%M:%S")
+#     # print(f'in MULTITEST iteration {i} out of {number_of_documents} with time {current_time}')
+#     i+=1
+#     f.write(f'{test}\n')
+# # print('no tests in these masters')
+# # print(masters_with_no_test)
+# #
+# # print('count of tests')
+# # print(count_tests)
+# #
+# # print('count of no tests')
+# # print(count_master_no_test)
+# f.close()
+#
+# now = datetime.now()
+#
+# current_time = now.strftime("%H:%M:%S")
+# print("after time =", current_time)
+# print('done')
+#
+#
+#
+#
+#
