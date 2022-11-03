fae569b0ee6e38b9655a51e2124d0c846de7c765
shayablaze <YeshayahuDov.Ajzner@ca.com>
2022-11-03 09:52:46 +0200

    stufff

--- torero-mig/get_billing_info.py
+++ torero-mig/get_billing_info.py
@@ -6,7 +6,6 @@ with open("props", "rb") as f:
     p.load(f, "utf-8")
 user = p.properties['user']
 password = p.properties['password']
-database_name =  'prod-main.ewwd2.mongodb.net/payments'
 database_address = 'prod-main.ewwd2.mongodb.net'
 database_name = 'payments'
 mongo_string2 = f'mongodb+srv://{user}:{password}@{database_address}/{database_name}'
@@ -17,7 +16,7 @@ cluster = MongoClient(mongo_string2)
 db = cluster['payments']
 collection = db["accounts"]
 
-temp = collection.find( {"$and":[{"uid": 'a-773774' }]}  )
+temp = collection.find( {"$and":[{"uid": {'$in':['a-773774']} }]}  )
 
 for doc in temp:
     print(doc['email'])
