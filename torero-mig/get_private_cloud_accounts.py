7346c0dcb8007dab8e9c57a16a030c678e0b1270
shayablaze <YeshayahuDov.Ajzner@ca.com>
2022-11-08 10:17:53 +0200

    not deleted

--- torero-mig/get_private_cloud_accounts.py
+++ torero-mig/get_private_cloud_accounts.py
@@ -84,7 +84,7 @@ print(len(projects_to_exclude))
 collection = db['tests']
 
 start = datetime(2022, 11, 6, 7, 1, 1)
-query = {"$and":[{"deleted": {"$exists":False }}, { "configuration.scriptType": {"$nin" : [ "jmeter", "taurus"  ]}    }, {"lastRunTime": {"$gte":start} }]}
+query = {"$and":[{"deleted": {"$exists":False }}, { "configuration.scriptType": {"$in" : [ "jmeter", "taurus"  ]}    }, {"lastRunTime": {"$gte":start} }]}
 
 tests_from_db = collection.find(   query)
 
