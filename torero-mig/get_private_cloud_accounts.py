8e3226093af5f9486005c9cfdee570a1483eac20
shayablaze <YeshayahuDov.Ajzner@ca.com>
2022-11-10 08:52:28 +0200

    get number of tests for this

--- torero-mig/get_private_cloud_accounts.py
+++ torero-mig/get_private_cloud_accounts.py
@@ -33,14 +33,14 @@ accounts_with_private_cloud = collection.find({"subscriptions.plan" : {"$in": pl
 i = 1
 account_ids = []
 for acc in accounts_with_private_cloud:
-    print(f'{i})')
-    print(acc['uid'])
-    print(acc['email'])
-    print('')
+    # print(f'{i})')
+    # print(acc['uid'])
+    # print(acc['email'])
+    # print('')
     account_ids.append(  int(acc['uid'].replace('a-', '')))
     i+=1
-print('here are account ids')
-print(account_ids)
+# print('here are account ids')
+# print(account_ids)
 #### to blazemeter database
 
 database_name = 'blazemeter'
@@ -61,8 +61,8 @@ workspaces_to_exclude = []
 for acc in accounts_from_blazemeter:
     print(acc['workspaces'])
     workspaces_to_exclude = list(set(workspaces_to_exclude) | set(acc['workspaces']))
-print('FROM BLAZEMETER ACCOUNTS HERE are the workspaces')
-print(workspaces_to_exclude)
+# print('FROM BLAZEMETER ACCOUNTS HERE are the workspaces')
+# print(workspaces_to_exclude)
 
 
 collection = db['projects']
@@ -76,34 +76,25 @@ projects_to_exclude = []
 for proj in projects_from_blaze:
     projects_to_exclude.append(proj['_id'])
 
-print('FROM BLAZEMETER PROJECT to exclude')
-print(projects_to_exclude)
-print(len(projects_to_exclude))
+# print('FROM BLAZEMETER PROJECT to exclude')
+# print(projects_to_exclude)
+# print(len(projects_to_exclude))
 
 
 collection = db['tests']
 
 start = datetime(2022, 5, 8, 7, 1, 1)
-start = datetime(2022, 11, 14, 7, 1, 1)
-query = {"$and":[{"project": {"$nin":projects_to_exclude}}, {"deleted": {"$exists":False }}, {"lastRunTime": {"$exists":True }}, { "configuration.scriptType": {"$in" : [ "jmeter", "taurus"  ]}    }, {"lastRunTime": {"$gte":start} }]}
+end = datetime(2022, 11, 14, 7, 1, 1)
+query = {"$and":[{"project": {"$nin":projects_to_exclude}}, {"deleted": {"$exists":False }}, {"lastRunTime": {"$exists":True }}, { "configuration.scriptType": {"$in" : [ "jmeter", "taurus"  ]}    }, {"lastRunTime": {"$gte":start , "$lte":end } }]}
 
 tests_from_db = collection.find(   query)
 
-print('printing tests')
-i = 1
+# print('printing tests')
+# i = 1
 # for test in tests_from_db:
 #     test_name=test['name']
 #     configuration = test['configuration']
-#     print(f'{i}) {test_name}')
-#     test_id = test['_id']
-#     if "scriptType" in configuration:
-#         if  configuration["scriptType"] =="" :
-#             print(f'this test has empty scriptType {test_id}')
-#         print(configuration["scriptType"])
-#     else:
-#
-#         print(f'whats here {test_id}')
-#
+#     print(f'test name : {i}) {test_name}')
 #     i+=1
 number_of_tests = collection.count_documents(query)
 print(f'number of tests is {number_of_tests}')
