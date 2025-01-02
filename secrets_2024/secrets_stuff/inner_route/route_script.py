c8771ae608eeefe78cc42a251d59629b480f781d
yajzner <yajzner@perforce.com>
2025-01-02 12:11:37 +0200

    secrets 2024

--- secrets_2024/secrets_stuff/inner_route/route_script.py
+++ secrets_2024/secrets_stuff/inner_route/route_script.py
@@ -2,13 +2,18 @@ from flask import Flask
 import threading
 import time
 import requests
-
+import asyncio
+from mitmproxy import options
+from mitmproxy.tools import dump
 # Flask app to handle incoming requests
 app = Flask(__name__)
 
+counter = 1
 @app.route('/do-this', methods=['GET'])
 def do_this():
-    print("Hiiiiiii!!!!!!")
+    global counter
+    counter += 1
+    print(f"Hiiiiiii!!!!!! and here is counter {counter}")
     return "OK", 200
 
 def run_flask():
@@ -21,12 +26,39 @@ def periodic_caller():
             print(f"Called yyyyy /do-this: {response.status_code}")
         except Exception as e:
             print(f"Error calling /do-this: {e}")
-        time.sleep(20)
+        time.sleep(200)
+
+class RequestLogger:
+    def request(self, flow):
+        print('whats up i am here finally')
+        # host = flow.request.headers['host']
+        print(flow.request)
+        print(flow.request.headers['Host'])
+
+async def start_proxy(host, port):
+    opts = options.Options(listen_host=host, listen_port=port)
+
+    master = dump.DumpMaster(
+        opts,
+        with_termlog=False,
+        with_dumper=False,
+    )
+    master.addons.add(RequestLogger())
+
+    await master.run()
+    return master
 
 if __name__ == "__main__":
-    # Start the Flask app in a separate thread
+
     flask_thread = threading.Thread(target=run_flask, daemon=True)
     flask_thread.start()
+    print('zzz starting the proxy')
+    asyncio.run(start_proxy('127.0.0.1', 4444))
+    print('after starting the server')
+
+
+    # Start the Flask app in a separate thread
+
 
     # Start the periodic caller
-    periodic_caller()
+    # periodic_caller()
--- secrets_2024/secrets_stuff/inner_route/route_thing_1_build_me.sh
+++ secrets_2024/secrets_stuff/inner_route/route_thing_1_build_me.sh
@@ -1,2 +1,2 @@
 docker build . -t route_thing
-docker run -it  route_thing
+docker run -it -e HTTP_PROXY=http://localhost:4444 -e HTTPS_PROXY=http://localhost:4444 -e REQUESTS_CA_BUNDLE=/root/.mitmproxy/mitmproxy-ca-cert.pem route_thing
