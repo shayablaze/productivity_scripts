66021b7dff95bc3720eea1b2722b9de79cb02201
yajzner <yajzner@perforce.com>
2025-01-27 10:34:59 +0200

    secrets 2024

--- secrets_2024/secrets_stuff/inner_route/me_requesting.py
+++ secrets_2024/secrets_stuff/inner_route/me_requesting.py
@@ -2,3 +2,4 @@ import requests
 print('inside me requesting')
 response = requests.get('http://localhost:5555/do-this')
 print(f"Called mememememememmememememmemememe /do-this: {response.status_code}")
+print(f"Called with response /do-this: {response.text}")
