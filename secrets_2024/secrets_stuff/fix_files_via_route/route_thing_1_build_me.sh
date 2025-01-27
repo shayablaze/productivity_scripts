8050852f1fccc8d51b98d8c1423a12d5516dcbcd
yajzner <yajzner@perforce.com>
2025-01-27 11:36:50 +0200

    secrets 2024

--- secrets_2024/secrets_stuff/fix_files_via_route/route_thing_1_build_me.sh
+++
@@ -1,2 +0,0 @@
-docker build . -t route_thing
-docker run -it -e HTTP_PROXY=http://localhost:4444 -e HTTPS_PROXY=http://localhost:4444 -e REQUESTS_CA_BUNDLE=/root/.mitmproxy/mitmproxy-ca-cert.pem route_thing
---
+++ secrets_2024/secrets_stuff/fix_files_via_route/switch_secrets_in_file.sh
@@ -0,0 +1,2 @@
+docker build . -t switch_secrets_in_file
+docker run -it -e HTTP_PROXY=http://localhost:4444 -e HTTPS_PROXY=http://localhost:4444 -e REQUESTS_CA_BUNDLE=/root/.mitmproxy/mitmproxy-ca-cert.pem switch_secrets_in_file
